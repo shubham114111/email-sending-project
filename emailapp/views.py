8c66f07525e703370e945f36880b760e804a4a5f
shubham114111 <sshubham681@gmail.com>
2021-01-23 11:49:10 +0530

    coding!

--- emailapp/views.py
+++ emailapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from . import forms
+from emailproject.settings import EMAIL_HOST_USER
+
+from django.core.mail import send_mail
 
 def send(request):
     obj = forms.Send()
+    if request.method == 'POST':
+        obj=forms.Send(request.POST)
+        subject = 'An email from Django'
+        message = 'This is sending from django'
+        recepient = str(obj['email'].value()) 
+        send_mail(subject,message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+        return render(request, 'emailapp/success.html', {'recepient': recepient})
+
     return render(request, 'emailapp/index.html', {'form':obj})
--- emailproject/settings.py
+++ emailproject/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#for Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
